3638541342351cfbbdfc94b538c4a0fdeb8c4f06
Solvejg Bougeois <s.bougeois@gmail.com>
2011-11-06 18:15:45 +0100

    create sandbox tree

---
+++ sandbox/testQt/src/HelloForm.cpp
@@ -0,0 +1,16 @@
+/*
+ * File:   HelloForm.cpp
+ * Author: Julien
+ *
+ * Created on 5 novembre 2011, 23:41
+ */
+
+#include "HelloForm.h"
+
+HelloForm::HelloForm() {
+    widget.setupUi(this);
+}
+
+HelloForm::~HelloForm() {
+}
+
