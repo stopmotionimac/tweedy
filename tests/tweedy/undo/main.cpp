1bb889b03bb50fb2f7f34d1db9ed8501abb9c968
Fabien Castan <fabien.castan@free.fr>
2011-11-08 23:05:45 +0100

    stupid example to check std::queue

--- tests/tweedy/undo/main.cpp
+++ tests/tweedy/undo/main.cpp
@@ -1,6 +1,7 @@
 #include <tweedy/core/truc.hpp>
 
 #include <iostream>
+#include <queue>
 
 #define BOOST_TEST_MODULE tweedy_undo
 #include <boost/test/unit_test.hpp>
@@ -12,8 +13,21 @@ BOOST_AUTO_TEST_CASE( undo_integers )
 {
 	// simple example to manipulate a stack of undo/redo commands
 	// with basic commands to manipulate integer, strings, ...
+	std::queue<int> q;
+	
+	q.push(5);
+	q.push(51);
+	q.push(52);
+	q.push(53);
 
-	BOOST_CHECK_EQUAL( 4.0/2.0, 6.0/3.0 );
+	BOOST_CHECK_EQUAL( q.front(), 5 );
+
+	q.pop();
+	
+	BOOST_CHECK_EQUAL( q.size(), 3 );
+	BOOST_CHECK_EQUAL( q.front(), 51 );
+	BOOST_CHECK_EQUAL( q.back(), 53 );
+	
 }
 
 
