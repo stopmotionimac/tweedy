bfcf82bc6054f343017147ddf22242a5b34e5f2d
Solvejg Bougeois <s.bougeois@gmail.com>
2011-12-10 02:01:53 +0100

    start of core Timeline's class

---
+++ src/tweedy/core/Clip.hpp
@@ -0,0 +1,18 @@
+#ifndef __CLIP_HPP__
+#define __CLIP_HPP__
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+class Clip {
+protected :
+        std::string imgPath;
+        float imgLength;
+        unsigned int imgwidth;
+        unsigned int imgHeight;
+        unsigned int imgreduction;
+};
+
+#endif
---
+++ src/tweedy/core/Timeline.hpp
@@ -0,0 +1,22 @@
+#ifndef __TIMELINE_HPP__
+#define __TIMELINE_HPP__
+
+#include "PisteClip.hpp"
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+class Timeline {
+private :
+        std::vector<PisteClip> listPiste;
+
+public :
+        void getlistPiste ();
+        void addPiste (PisteClip & pisteClip);
+        void supprPiste (PisteClip & pisteClip);
+
+};
+
+
+#endif
