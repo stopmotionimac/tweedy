e848e0050ac74de4b14adf380883ab597fcb79ea
Guillaume Bonvoust <g.bonvoust@gmail.com>
2011-12-22 15:39:19 +0100

    fix

---
+++ sandbox/testUndo/src/UndoView.hpp
@@ -0,0 +1,60 @@
+#ifndef UNDOVIEW_HPP
+#define	UNDOVIEW_HPP
+
+#include "IUndoRedoCommand.hpp"
+#include "CommandManager.hpp"
+
+#include <boost/ptr_container/ptr_vector.hpp>
+#include <QtGui/QListView>
+#include <QtGui/QUndoGroup>
+#include <QtGui/QMainWindow>
+#include <QtGui/QListWidget>
+#include <QtGui/QWidget>
+#include <QtGui/QVBoxLayout>
+#include <QtGui/QPushButton>
+#include <QtGui/QLabel>
+
+
+class UndoView : public QListWidget{
+    Q_OBJECT
+public:
+        UndoView(QWidget* parent =0);
+        UndoView(CommandManager * cmdManager, QWidget * parent = 0) 
+                : QListWidget(parent), cmdMan(cmdManager){
+        }
+        
+        UndoView(QUndoGroup * group, QWidget * parent = 0): undoGroup(group){}
+        
+        ~UndoView();
+
+        QIcon cleanIcon() const;
+        void setCleanIcon(const QIcon & icon);
+        
+        QString	getEmptyLabel () const;
+        void setEmptyLabel( const QString & label );
+        
+        QUndoGroup * group() const;
+        
+        std::stack<IUndoRedoCommand*>& stack() const;
+        
+        void fillStringUndoStack();
+        void fillStringRedoStack();
+        void fill();
+
+        CommandManager* getCmdMan();
+
+private Q_SLOTS:
+    //void setGroup ( QUndoGroup * group);
+    //void setStack ( boost::ptr_vector<IUndoRedoCommand>* undoStack);
+    
+private:
+       
+    QIcon icon;
+    QString emptyLabel;
+    CommandManager* cmdMan;
+    QUndoGroup * undoGroup;
+    std::stack<QString> pileString;
+        
+};
+#endif	/* UNDOVIEW_HPP */
+
--- src/tweedy/core/Imedia.hpp
+++ src/tweedy/core/Imedia.hpp
@@ -1,10 +1,18 @@
 #ifndef IMEDIA_HPP
 #define IMEDIA_HPP
 
+#include<string>
+
 class Imedia
 {
 public:
     Imedia();
+
+protected:
+    int IdMedia;
+    std::string name;
 };
+
+
 
 #endif // IMEDIA_HPP
