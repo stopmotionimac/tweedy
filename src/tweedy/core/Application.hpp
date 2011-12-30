38ac1b8d28f8a7c590da101130656b944d74f468
Solvejg Bougeois <s.bougeois@gmail.com>
2011-12-30 19:05:49 +0100

    distingue Singleton class and Application class

--- src/tweedy/core/Application.hpp
+++ src/tweedy/core/Application.hpp
@@ -1,57 +1,12 @@
 #ifndef APPLCIATION_HPP
 #define APPLICATION_HPP
 
+#include "Singleton.hpp"
 #include "Imedia.hpp"
 
 #include<iostream>
 #include <boost/ptr_container/ptr_vector.hpp>
 
-//
-//Singleton to any class
-//
-
-template <typename T>
-class Singleton
-{
-protected:
-  // Constructor/destructor
-  Singleton () { }
-  ~Singleton () { std::cout << "destroying singleton." << std::endl; }
-
-public:
-  static T *getInstance ()
-  {
-    if (NULL == _singleton)
-      {
-        std::cout << "creating singleton." << std::endl;
-        _singleton = new T;
-      }
-    else
-      {
-        std::cout << "singleton already exist" << std::endl;
-      }
-
-    return (static_cast<T*> (_singleton));
-  }
-
-  static void kill ()
-  {
-    if (NULL != _singleton)
-      {
-        delete _singleton;
-        _singleton = NULL;
-      }
-  }
-
-private:
-  static T *_singleton;
-};
-
-template <typename T>
-T *Singleton<T>::_singleton = NULL;
-
-//
-//
 
 class Application : public Singleton<Application>
 {
---
+++ src/tweedy/core/Singleton.hpp
@@ -0,0 +1,50 @@
+#ifndef SINGLETON_HPP
+#define SINGLETON_HPP
+
+#include<iostream>
+#include <boost/ptr_container/ptr_vector.hpp>
+
+//
+//Singleton to any class
+//
+
+template <typename T>
+class Singleton
+{
+protected:
+  // Constructor/destructor
+  Singleton () { }
+  ~Singleton () { std::cout << "destroying singleton." << std::endl; }
+
+public:
+  static T *getInstance ()
+  {
+    if (NULL == _singleton)
+      {
+        std::cout << "creating singleton." << std::endl;
+        _singleton = new T;
+      }
+    else
+      {
+        std::cout << "singleton already exist" << std::endl;
+      }
+
+    return (static_cast<T*> (_singleton));
+  }
+
+  static void kill ()
+  {
+    if (NULL != _singleton)
+      {
+        delete _singleton;
+        _singleton = NULL;
+      }
+  }
+
+private:
+  static T *_singleton;
+};
+
+template <typename T>
+T *Singleton<T>::_singleton = NULL;
+#endif  // SINGLETON_HPP
