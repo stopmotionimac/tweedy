5263cc798b3d0c2b3e206618f2ee68fb73dbb170
Solvejg Bougeois <s.bougeois@gmail.com>
2012-05-12 19:55:07 +0200

    add pictures captured on map in core

--- src/tweedy/gui/mainWindow/MainWindow.cpp
+++ src/tweedy/gui/mainWindow/MainWindow.cpp
@@ -346,7 +346,9 @@ void MainWindow::on_captureAction_triggered()
 
 		_chutier->getTabWidget().setCurrentWidget( &listWidgetCapture );
 		/*add to chutier core*/
-
+                boost::filesystem::path nameOfFileToImport( filenameLD );
+                ChutierMediaExt& chutierPictures = project.getChutierPictures();
+                chutierPictures.importMediaToChutier( nameOfFileToImport);
 	}
 
 }
