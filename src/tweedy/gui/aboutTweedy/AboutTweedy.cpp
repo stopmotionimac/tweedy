6e564c1127a1a968e5756a41c2dad83fd46dfda0
Stephanie <nini_6905@hotmail.fr>
2012-05-14 10:36:37 +0200

    about tweedy

--- src/tweedy/gui/aboutTweedy/AboutTweedy.cpp
+++ src/tweedy/gui/aboutTweedy/AboutTweedy.cpp
@@ -10,7 +10,11 @@ AboutTweedy::AboutTweedy(QWidget *parent) :
     _ui->setupUi(this);
     _ui->logoTWEEDYLabel->setPixmap(QString::fromStdString("img/logo.png"));
     _ui->logoIMACLabel->setPixmap(QString::fromStdString("img/imac.jpg"));
-
+    QPixmap team = QString::fromStdString("img/lateam.JPG");
+    _ui->photoLabel->setPixmap(team.scaled(450,300));
+    _ui->photoLabel->setFixedSize(450,300);
+    _ui->photoLabel->setAlignment (Qt::AlignCenter);
+    this->setFixedSize(470,580);
 }
 
 AboutTweedy::~AboutTweedy()
